41fd992547432a90f332504ec8d7f2f6663f14a3
adigine959 <adiginezh@gmail.com>
2022-11-20 01:30:01 +0600

    добавил модели Post, и Hashtag ,

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Hashtag(models.Model):
+    title = models.CharField(max_length=500)
+    posts = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+    likes = models.IntegerField()
+    hashtags = models.ForeignKey(Hashtag, blank=True)
+
+    def __str__(self):
+        return self.title
