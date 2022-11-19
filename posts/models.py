4ee985b8114f88b316e4b02ee33910dcb53d5aac
adigine959 <adiginezh@gmail.com>
2022-11-20 02:53:12 +0600

    добавил новое поле для модели Post

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 
 class Hashtag(models.Model):
@@ -19,3 +19,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.author.username}, {self.text}'
+
